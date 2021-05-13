08a9b074c85e6808f2a4c09630d6c4282f332b03
MATTY2401 <bernardo.1939439@studenti.uniroma1.it>
2021-05-13 16:43:23 +0200

    classe Storage per la creazione dell'albero

---
+++ src/com/company/Storage.java
@@ -0,0 +1,49 @@
+package com.company;
+import webscraper.*;
+import java.util.*;
+
+import java.util.ArrayList;
+
+public class Storage {
+    private ArrayList<Dinasty> dynasties;
+    private WebScraper scraber=new WebScraper();
+    private Set<String> trovati;
+    private Set<String> emperors;
+
+    public Storage(String url){
+        ArrayList<Dinasty> dynasties = scraber.getDynasties(url);
+    }
+
+    public Member getTree(String nameDinasty){
+        for(int i=0;i<dynasties.size();i++){
+            if(dynasties.get(i).getName().equals(nameDinasty)){
+                return search(dynasties.get(i));
+            }
+        }
+        return null;
+    }
+
+    private Member search(Dinasty dinasty){
+        Member emperor = dinasty.getMembers().get(0);
+        for(int i=0;i<dinasty.getMembers().size();i++){
+            emperors.add(dinasty.getMembers().get(i).getUrl());
+        }
+        ricorsione(emperor);
+        if(emperors.isEmpty()){
+            return emperor;
+        }
+        //se devo creare 2 rami scollegati da aggiungere
+    }
+
+    private void ricorsione(Member emperor){
+        scraber.addMemberInfo(emperor);
+        for(int i=0;i<emperor.getIssue().size();i++){
+            if(emperors.remove(emperor.getUrl())){
+                emperor.setEmperor(true);
+            }
+            if(trovati.add(emperor.getIssue().get(i).getUrl())) {
+                ricorsione(emperor.getIssue().get(i));
+            }
+        }
+    }
+}
