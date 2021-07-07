4b5f4a75aa9f38bff62683399ff43707785cc02f
Metgraph <loydmajk61@gmail.com>
2021-07-07 19:43:10 +0200

    fix WebScraperTester

--- src/webscraper/WebScraperTester.java
+++ src/webscraper/WebScraperTester.java
@@ -13,75 +13,61 @@ public class WebScraperTester {
 
 
     public static void testWebScraper() {
-        WebScraper ws;
-        try{
-            ws = new WebScraper("resources/chromedriversadfs.exe");
-            System.out.println("Test errore driver fallito");
-            return;
-        }catch (IllegalStateException | WebDriverException e){
-            System.out.println("Test errore driver passato");
-            ws = new WebScraper("resources/chromedriver.exe");
-        }
+        WebScraper ws = new WebScraper("resources/chromedriver.exe");
 
-        try{
-            ArrayList<Dynasty> arr = ws.getDynasties("https://it.wikipedia.net/wiki/Imperatori_romani");
-            System.out.println("Test errore driver fallito");
-            return;
-        }catch (IllegalArgumentException e){
-            System.out.println("Test errore link passato");
-        }
+        testURL(ws);
+
         ArrayList<Dynasty> arr = ws.getDynasties("https://it.wikipedia.org/wiki/Imperatori_romani");
 
+        testPrelevazioneDati(arr, ws);
+
+        ws.close();
+
+    }
+
+
+    private static void testURL(WebScraper ws) {
         try {
-            test1(arr, ws);
-            test2(arr, ws);
-            System.out.println("Test scraping effettuati");
+            ArrayList<Dynasty> arr = ws.getDynasties("https://it.wikipedia.net/wiki/Imperatori_romani");
+            System.out.println("Test errore URL fallito");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Test errore URL passato");
         } catch (Exception e) {
-            System.out.println("Test scraping fallito:\n" + e);
-        } finally {
-            ws.close();
-        }
-
-    }
-
-    public static void test1(ArrayList<Dynasty> arr, WebScraper ws) {
-        Member m = arr.get(0).getMembers().get(0);
-        ws.addMemberInfo(m);
-        System.out.println("\nStampa della lista");
-        System.out.println(m.getIssue());
-        System.out.println("Madre: " + m.getMother());
-        System.out.println("Padre: " + m.getFather());
-        System.out.println("Coniuge: " + m.getSpouses());
-        System.out.println();
-
-        Member m2 = arr.get(1).getMembers().get(0);
-        ws.addMemberInfo(m2);
-        System.out.println("\nStampa della lista");
-        System.out.println(m2.getIssue());
-        System.out.println("Madre: " + m2.getMother());
-        System.out.println("Padre: " + m2.getFather());
-        System.out.println("Coniuge: " + m2.getSpouses());
-        System.out.println();
-        System.out.println("fine");
-    }
-
-    public static void test2(ArrayList<Dynasty> arr, WebScraper ws) {
-        for (Dynasty dynasty : arr) {
-            System.out.println(dynasty.getName());
-            ArrayList<Member> members = dynasty.getMembers();
-            for (Member member : members) {
-                ws.addMemberInfo(member);
-                System.out.println("Imperatore: " + member);
-                System.out.println("\nStampa della lista");
-                System.out.println(member.getIssue());
-                System.out.println("Madre: " + member.getMother());
-                System.out.println("Padre: " + member.getFather());
-
-                System.out.println("Coniuge: " + member.getSpouses());
-                System.out.println("-------------------\n");
-            }
+            System.out.println("Test errore URL fallito:\n" + e);
         }
     }
 
+    public static void testPrelevazioneDati(ArrayList<Dynasty> arr, WebScraper ws) {
+        try {
+            System.out.println("Stampa della famiglia prevista");
+            Member m = arr.get(0).getMembers().get(0);
+            ws.addMemberInfo(m);
+            System.out.println("\nStampa della lista");
+            System.out.println(m.getIssue());
+            System.out.println("Madre: " + m.getMother());
+            System.out.println("Padre: " + m.getFather());
+            System.out.println("Coniuge: " + m.getSpouses());
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println("Test prelevazione fallito:\n" + e);
+        }
+        try {
+
+            System.out.println("Stampa della famiglia non prevista, dovrebbe tornare errore");
+            Member m2 = new Member("paolo", "nrlikwsaef");
+            ws.addMemberInfo(m2);
+            System.out.println("\nStampa della lista");
+            System.out.println(m2.getIssue());
+            System.out.println("Madre: " + m2.getMother());
+            System.out.println("Padre: " + m2.getFather());
+            System.out.println("Coniuge: " + m2.getSpouses());
+            System.out.println();
+            System.out.println("Test prelevazione fallito, il programma avrebbe dovuto generare errore");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Test prelevazione eseguito con successo");
+        } catch (Exception e) {
+            System.out.println("Test prelevazione fallito:\n" + e);
+        }
+    }
 
 }
